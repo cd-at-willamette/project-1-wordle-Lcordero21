3edc4f5858eb9157272ead8781aab6115ccff190
Lcordero21 <lcordero@willamette.edu>
2024-09-28 10:53:57 -0700

    WIP Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -1,8 +1,8 @@
 ########################################
-# Name: Lola Corder
-# Collaborators (if any): went to QUAD Center
+# Name: Lola Cordero
+# Collaborators (if any): Went to the Quad Center for milestone 0
 # GenAI Transcript (if any):
-# Estimated time spent (hr): 1
+# Estimated time spent (hr): 3-4
 # Description of any added extensions:
 ########################################
 
@@ -13,24 +13,65 @@ import random
 
 def wordle():
     # The main function to play the Wordle game.
-    row=0 
-    
-    
-    def display_word(word):
-        for i in range(N_COLS):
-            letter=word[i]
-            gw.set_square_letter(0,i,letter)
+    secretWord= "Tacos"
 
-        
     def enter_action():
-        # What should happen when RETURN/ENTER is pressed.
-        gw.show_message("You need to implement this method")
+        if (len(guess(gw.get_current_row())) < 5) and (check_if_english() == True):
+            gw.show_message("Word must be at least 5 letters!") #This is will check to make sure word is more then 5 letters
+        if check_if_english() == False:
+            gw.show_message("Not in Word List Buddy-ol-pal") #This will make sure that the word is in the english list
+        else:
+            if gw.get_current_row!=N_ROWS:
+                color_squares(guess(gw.get_current_row()))
+                gw.set_current_row(gw.get_current_row() + 1) #If guess meets criteria then it will proceed
+
+
+        # add check_color function!!
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
-    secret_word="Happy"
-    display_word(secret_word)
+
     
+
+
+    def check_if_english():
+        guess_str= guess(gw.get_current_row())
+        guess_lower= guess_str.lower()
+        status=False
+        for i in range(len(ENGLISH_WORDS)):
+            if guess_lower==ENGLISH_WORDS[i]:
+                status=True
+        return status
+    
+    def guess(row:int) -> str:
+        temp=""
+        for i in range(N_COLS):
+            temp= temp + gw.get_square_letter(row,i)
+        return temp
+    
+    def color_squares(guess):
+        lettersLeft= secretWord
+        for i in range (N_COLS):
+            if guess[i] == lettersLeft[i]:
+                lettersLeft=lettersLeft[:i]+" "+lettersLeft[i+1:]
+                gw.set_square_color(gw.get_current_row(),i, CORRECT_COLOR)
+        for i in range (N_COLS):
+            if lettersLeft[i] != " ":
+                if guess[i] in lettersLeft:
+                    lettersLeft=lettersLeft[:i]+" "+lettersLeft[i+1:]
+                    gw.set_square_color(gw.get_current_row(),i, PRESENT_COLOR)
+                else:
+                    gw.set_square_color(gw.get_current_row(),i,MISSING_COLOR)
+
+
+            
+
+
+
+# Startup boilerplate
+if __name__ == "__main__":
+    wordle()
+
 
 # Startup boilerplate
 if __name__ == "__main__":
